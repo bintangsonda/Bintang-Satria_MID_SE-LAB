c2dc9040f7cbb4febbbf880996b5e9a8fd992213
bintangsonda <bintangsonda27@gmail.com>
2022-03-08 21:30:32 +0700

    Demo Commit

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import StudiKasus2
-import os
-import pandas as pd
 
-case = StudiKasus2.StudiKasus2('localhost', '3306', 'root', '')
-df = case.import_csv('username.csv')
-print(case.create_db('nameUs3'))
-print(case.create_table('nameUs3', 'The_Data', df))
-print(case.load_data('nameUs3', 'The_Data'))
-print("did it")
+if __name__ == "__main__":
+    case = StudiKasus2.StudiKasus2('localhost', '3306', 'root', '')
+    text = open("username.csv", "r")
+    text = ''.join([i for i in text]) \
+        .replace(";", ",")
+    x = open("username.csv","w")
+    x.writelines(text)
+    x.close()
+    
+    df = case.import_csv("username.csv")
+    print(case.create_db('midtermse'))
+    print(case.create_table('midtermse', 'username', df))
+    print(case.load_data('midtermse', 'username'))
+    print("did it")
+
+
